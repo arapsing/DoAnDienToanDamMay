a5643b37fba0b5fec6562813c8f6f8c4ef9886ca
Khánh Nguyễn <20110233@student.hcmute.edu.vn>
2022-11-30 03:02:33 +0700

    base-dir

--- WebNS/settings.py
+++ WebNS/settings.py
@@ -75,7 +75,7 @@ ROOT_URLCONF = 'WebNS.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS':[os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
